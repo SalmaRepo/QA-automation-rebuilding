0b63b2e00f86677d0430547599484f4cfe9dc002
Salma Sheik <salma.shaikh751@gmail.com>
2026-09-02 07:08:47 +0530

    Added:single select dropdowns

---
+++ week-02-playwright-actions/tests/dropdown_validations.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from "@playwright/test";
+
+const pageUrl = "https://sdetqa.vercel.app/autoplay.html";
+
+test.describe("Handling dropdowns", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(pageUrl);
+    await expect(page.getByText("Autoplay")).toBeVisible();
+  });
+
+  test("1.Page Load Validation", async ({ page }) => {
+    //open the URL and verify the page loaded
+    await expect(page).toHaveURL("https://sdetqa.vercel.app/autoplay");
+    //verify the autoplay heading is visible
+    await expect(page.getByText(" Dropdowns & more")).toBeVisible();
+  });
+
+  //single dropdown validations
+
+  test("single select dropdown validations", async ({ page }) => {
+    //Locate Country dropdown Dropdown should be visible
+    const countryDropdown = page.locator("#country");
+    await expect(countryDropdown).toBeVisible();
+    //Get default selected value Default should be India (india)
+    await expect(countryDropdown).toHaveValue("india");
+
+    //select by visible label
+    await countryDropdown.selectOption({ label: "USA" });
+    // await countryDropdown.selectOption('USA');
+    await expect(countryDropdown).toHaveValue("usa");
+
+    //select by option value
+    await countryDropdown.selectOption({ value: "uk" });
+    await expect(countryDropdown).toHaveValue("uk");
+    //select by index
+    await countryDropdown.selectOption({ index: 3 });
+    await expect(countryDropdown).toHaveValue("germany");
+    //select by combination of value and label
+    await countryDropdown.selectOption({ value: "france", label: "France" });
+    await expect(countryDropdown).toHaveValue("france");
+
+    //Count total dropdown options Count should be 5
+    const options = countryDropdown.locator("option");
+    await expect(options).toHaveCount(5);
+
+    const optionsMul = page.locator("#country option");
+    await expect(optionsMul).toHaveCount(5);
+
+    //validate options list contains germany
+    const optionsTexts = await options.allInnerTexts(); //capture text from all the options and retuns string array
+    //options.allInnerTexts()-also includes special characters
+    expect(optionsTexts).toContain("Germany");
+
+    for (const option of optionsTexts) {
+      console.log(option);
+    }
+  });
+});
